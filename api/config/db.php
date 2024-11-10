0f4ac4990a7d0dc08bf206add0cd02a0c72b1246
Jhonier <jhonier.becerra@zoomcem.com>
2024-11-09 23:26:26 -0500

    Signin - add connection with js

--- api/config/db.php
+++ api/config/db.php
@@ -11,8 +11,7 @@ class Database {
 
         try {
             $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexión exitosa";
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);            
         } catch(PDOException $e) {
             echo "Error de conexión: " . $e->getMessage();
         }
